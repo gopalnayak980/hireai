af18135e74d84c188a6f113a5be32c201acf0723
Gopal_Nayak <gopal32452@gmail.com>
2026-06-30 10:58:06 +0530

    Update API URL for production

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://hireai-backend-u87f.onrender.com/api'
 })
 
-// Token automatically add karo har request mein
 API.interceptors.request.use(req => {
   const token = localStorage.getItem('token')
   if (token) req.headers.Authorization = `Bearer ${token}`
